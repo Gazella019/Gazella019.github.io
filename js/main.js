33373bf2a2cd895321614d756a62ca2e69e281b7
Gazella019 <edison19951219@gmail.com>
2020-08-28 18:03:02 +0800

    personal website

---
+++ js/main.js
@@ -0,0 +1,55 @@
+
+
+
+
+$('#blog-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
+
+
+
+
+$('#gallery-carousel').owlCarousel({
+    loop:true,
+    autoplay: true,
+    margin:10,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    nav:true,
+    autoplayHoverPause: true,
+    items: 1,
+    autoheight: true,
+    responsive:{
+      0:{
+        items:1,
+        nav:false
+      },
+      600:{
+        items:1,
+        nav:false
+      },
+      1000:{
+        items:1,
+        nav:true
+      }
+    }
+});
+
+AOS.init();
+
+
+
+
